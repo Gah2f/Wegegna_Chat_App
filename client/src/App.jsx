4b1ada44bf43b806ff43f40d2f5b7cfb8b3ee663
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-05-12 15:51:09 +0300

    Toaster added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,12 @@ import { Routes, Route  } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import ProfilePage from './pages/ProfilePage'
+import { Toaster } from 'react-hot-toast';
+
 function App() {
   return (
     <div className='bg-[url("./src/assets/chat-app-assets/bgImage.svg")] bg-contain'>
+      <Toaster/>
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
