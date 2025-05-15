fa1201d2097081fdde38d3ba6aa50e98aac37856
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-05-15 14:07:07 +0300

    Edited

--- server/server.js
+++ server/server.js
@@ -44,8 +44,14 @@ app.use("/api/messages", messageRouter);
 
 await conncetDB();
 
-const PORT = process.env.PORT || 5000;
 
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000; 
 server.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
 });
+
+}
+
+export default server 
